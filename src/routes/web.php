2a8f41b185386572093f0e120bcdb8c34956a4d7
João Ribeiro <joao.ribeiro@controlink.pt>
2021-01-07 10:47:40 +0000

    Namespace Updated

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
-use controlink\winmax4\Http\Controllers\ArticleController;
-use controlink\winmax4\Http\Controllers\FamilyController;
+use Controlink\Winmax4\Http\Controllers\ArticleController;
+use Controlink\Winmax4\Http\Controllers\FamilyController;
 
 Route::get('/getArticles', [ArticleController::class, 'get']);
 Route::get('/getFamilies', [FamilyController::class, 'get']);
